3aa92adb3a86a94b3ab6564d5c8c5aed093ff9e2
wanglaoea <marsyasasaputri495@gmail.com>
2025-07-06 21:27:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: ['https://front-b6xv618v8-huxla.vercel.app'],
+    origin: ['https://front-huxla.vercel.app'],
     methods: ['POST']
 }));
 
